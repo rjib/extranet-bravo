5db2846ef0cf6f60d27771f52ff56238ecb90bcb
ricardoalvarenga101 <ricardoalvarenga101@gmail.com>
2012-11-07 19:48:51 +0000

    persistencia e validações importacao .AC, assim como criaçao da nova tabela.

--- branches/bravo-1.0/extranet/sistema/models/tb_pcp_ac.php
+++ branches/bravo-1.0/extranet/sistema/models/tb_pcp_ac.php
@@ -13,21 +13,24 @@ class tb_pcp_ac{
 		$this->conexaoERP = $conexaoERP;
 
 	}
-
+	
 	/**
-	 * Metodo para listar todas os arquivos AC
+	 * Metodo para inserir novo arquivo AC e retornar o id
+	 * @param int $cod_pcp_ad
+	 * @param int $un_complementar
+	 * @return boolean
 	 * @author Ricardo S. Alvarenga
-	 * @since 31/10/2012
-	 * @return array
+	 * @since 07/11/2012
 	 */
-	public function listaTodosArquivos(){
-		try {
-			$sql = "SELECT CO_COR, DS_COR, CO_RECNO FROM tb_pcp_cor ORDER BY DS_COR ASC";
-			$row = mysql_query($sql,$this->conexaoERP);
-		}catch (Exception $e){
-			return false;
+	public function insertReturnId($cod_pcp_ad)
+	{
+		$sql = "INSERT INTO tb_pcp_ac (co_pcp_ad)
+				VALUES (".addslashes($cod_pcp_ad).")";
+		if(!mysql_query($sql,$this->conexaoERP)){
+			throw new Exception('Erro na inserção dos dados, favor contacte o suporte!');
 		}
-		return $row;
+		$id = mysql_insert_id();
+		return $id;
 	}
 }
 ?>
