72a906f3a219f85bcbb085e8a511113cf668c3ed
ricardoalvarenga101@gmail.com <ricardoalvarenga101@gmail.com@8a00c225-3f9b-5239-73f0-b633546a12c4>
2013-01-10 18:32:03 +0000

    limitando quantidade de registros por pagina 15 registros

--- branches/bravo-1.0/extranet/sistema/inc/cadastros/controle_acesso/acesso_consultor_grid.php
+++ branches/bravo-1.0/extranet/sistema/inc/cadastros/controle_acesso/acesso_consultor_grid.php
@@ -10,7 +10,7 @@
 	$paging->labels('C&oacute;digo,Data Cadastro,Hora Entrada,Hora Sa&iacute;da,Cart&atilde;o,Prestador de Servi&ccedil;o');
 	$paging->fields('ACESSO_CONSULTOR.CO_ACESSO_PRESTADOR,DT_CADAS,HR_ENTRADA,HR_SAIDA,NU_CARTAO_IDENTIFICACAO,NOME_PESSOA');
 	$paging->cols_width('60');
-	$paging->rowsperpage(30);
+	$paging->rowsperpage(15);
 	$paging->page(isset($_GET['p']) ? $_GET['p'] : 1);
 	
 	if($_GET['load'] == 'controls'){
