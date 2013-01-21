936eb297fc3bee9e37ffc7ac618c7fe943b57e82
ricardoalvarenga101@gmail.com <ricardoalvarenga101@gmail.com@8a00c225-3f9b-5239-73f0-b633546a12c4>
2013-01-21 11:29:36 +0000

    visualizar desenho da peça em apontamento.

---
+++ branches/bravo-1.0/extranet/sistema/ireport/relatorio/pcp_etiqueta_peca.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$data=false;
+require_once('../class/tcpdf/tcpdf.php');
+require_once("../class/PHPJasperXML.inc.php");
+//require_once("class/PHPJasperXMLSubReport.inc.php");
+require_once('../../setup.php');
+require_once '../../models/tb_pcp_etiqueta.php';
+require_once APP_PATH.'sistema/models/tb_pcp_pecas.php';
+
+date_default_timezone_set('America/Sao_Paulo');
+
+$nu_op			   = $_GET['nu_op'];
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+$server=DSN;
+$db="extranet";
+$user=USER;
+$pass=PASS;
+$version="0.8b";
+$pgport=5432;
+$pchartfolder="../class/pchart2";
+$timestamp = date("d/m/Y")."  ".date("h:i:s");
+$co_usuario = $_SESSION['codigoUsuario'];
+
+$_etiqueta = new tb_pcp_etiqueta($conexaoERP);
+$_etiqueta->proc_etiqueta_peca_relatorio($nu_op, $co_usuario);
+
+$xml =  simplexml_load_file("pcp_etiqueta_peca.jrxml");
+$PHPJasperXML = new PHPJasperXML();
+//$PHPJasperXML->debugsql=true;
+$PHPJasperXML->arrayParameter=array("CO_USUARIO"=>$co_usuario);
+$PHPJasperXML->xml_dismantle($xml);
+
+//$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db); * use this line if you want to connect with mysql
+
+//if you want to use universal odbc connection, please create a dsn connection in odbc first
+$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db);
+
+//$PHPJasperXML->outpage("F",APP_PATH.'barcodes'.DS.date("dmYhis").".pdf");    //page output method I:standard output  D:Download file
+$PHPJasperXML->outpage("I",date("dmYhis"));    //page output method I:standard output  D:Download file
+
+$_etiqueta->limparTemporariaEtiquetaPeca($co_usuario);
+
+$data=true;
+
+?>
