ae24c0fe865509135b686586d0eca41a74a65f7c
euripedes.junior <euripedes.junior@yahoo.com.br>
2013-02-05 15:54:48 +0000

    Acerto filtro por OP.

--- branches/bravo-1.0/extranet/sistema/inc/pcp/apontamento_grid.php
+++ branches/bravo-1.0/extranet/sistema/inc/pcp/apontamento_grid.php
@@ -6,11 +6,11 @@
 	$searchfor = isset($_GET['searchfor']) ? $_GET['searchfor'] : '';
 	
 	$paging->table('tb_pcp_apontamento PCP_APONTAMENTO');
-	$paging->where('PCP_RECURSO.NO_RECURSO = "'.$searchfor.'" OR CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) LIKE "%'.$searchfor.'%" AND PCP_RECURSO.FL_DELET IS NULL');
+	$paging->where('PCP_RECURSO.NO_RECURSO = "'.$searchfor.'" OR (CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) IS NULL OR CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) LIKE "%'.$searchfor.'%")');
 	$paging->labels('C&oacute;digo,Data Apontamento,Recurso,Hora Inicio,Hora Fim,Tipo Apontamento,Numero. OP');
 	$paging->fields('PCP_APONTAMENTO.CO_PCP_APONTAMENTO,DT_APONTAMENTO,NO_RECURSO,HR_INICIO,HR_FIM,FL_APONTAMENTO,NU_OP');
 	$paging->cols_width('60,120,200,70,70,200,100');
-	$paging->rowsperpage(15);
+	$paging->rowsperpage(30);
 	$paging->page(isset($_GET['p']) ? $_GET['p'] : 1);
 	
 	if($_GET['load'] == 'controls'){
