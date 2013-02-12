c54337ea5b3c827db4179e4527fe9dffc25d2120
ricardoalvarenga101 <ricardoalvarenga101@gmail.com>
2013-02-12 14:10:29 +0000

    correção grid apontamento por operador

--- extranet/sistema/inc/pcp/apontamento_grid.php
+++ extranet/sistema/inc/pcp/apontamento_grid.php
@@ -1,12 +1,13 @@
 <?php
-	
+	session_start();
+	$codigo_usuario = $_SESSION['codigoUsuario'];
 	require('apontamento_grid_paging.php');
 	$paging = new Paging();
 	
 	$searchfor = isset($_GET['searchfor']) ? $_GET['searchfor'] : '';
 	
 	$paging->table('tb_pcp_apontamento PCP_APONTAMENTO');
-	$paging->where('CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) IS NULL OR CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) LIKE "'.$searchfor.'%"');
+	$paging->where('(CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) IS NULL OR CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) LIKE "'.$searchfor.'%") ');
 	$paging->labels('C&oacute;digo,Data Apontamento,Recurso,Hora Inicio,Hora Fim,Tipo Apontamento,Numero. OP');
 	$paging->fields('PCP_APONTAMENTO.CO_PCP_APONTAMENTO,DT_APONTAMENTO,NO_RECURSO,HR_INICIO,HR_FIM,FL_APONTAMENTO,NU_OP');
 	$paging->cols_width('60,120,200,70,70,200,100');
