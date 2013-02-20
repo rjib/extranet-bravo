32a46829e22c6e67b684bab47431b5aa986bee1e
ricardoalvarenga101 <ricardoalvarenga101@gmail.com>
2013-02-20 14:39:41 +0000

    removendo descricao do grid de apontamento.

--- extranet/sistema/inc/pcp/apontamento_grid.php
+++ extranet/sistema/inc/pcp/apontamento_grid.php
@@ -8,9 +8,9 @@
 	
 	$paging->table('tb_pcp_apontamento PCP_APONTAMENTO');
 	$paging->where('(CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) IS NULL OR CONCAT(PCP_OP.CO_NUM, PCP_OP.CO_ITEM, PCP_OP.CO_SEQUENCIA) LIKE "'.$searchfor.'%") ');
-	$paging->labels('Desc. Prod.,Data Apontamento,Recurso,Hora Inicio,Hora Fim,Tipo Apontamento,Numero. OP, C&oacute;d. Int.');
-	$paging->fields('PCP_PRODUTO.DS_PRODUTO,DT_APONTAMENTO,NO_RECURSO,HR_INICIO,HR_FIM,FL_APONTAMENTO,NU_OP,PCP_PRODUTO.CO_INT_PRODUTO');
-	$paging->cols_width('200,120,200,70,70,10,80,60');
+	$paging->labels('C&oacute;digo,Data Apontamento,Recurso,Hora Inicio,Hora Fim,Tipo,Numero. OP, C&oacute;d. Int.');
+	$paging->fields('PCP_APONTAMENTO.CO_PCP_APONTAMENTO,DT_APONTAMENTO,NO_RECURSO,HR_INICIO,HR_FIM,FL_APONTAMENTO,NU_OP,PCP_PRODUTO.CO_INT_PRODUTO');
+	$paging->cols_width('50,120,200,70,80,50,90,60');
 	$paging->rowsperpage(10);
 	$paging->page(isset($_GET['p']) ? $_GET['p'] : 1);
 	
